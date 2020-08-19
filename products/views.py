3dd2ab3db72cf3baa125379e52737427860e4299
mkthewlis <mkthewlis@gmail.com>
2020-08-19 07:25:03 +0000

    added shopping bag app, urls and template

--- products/views.py
+++ products/views.py
@@ -4,7 +4,6 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
 
-# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
